6be2c3b0f2dcdffff56e93d7b3fffef75cf00d8a
Rizwan Liaqat <rizwanliaqat97@gmail.com>
2022-04-20 14:22:36 +0500

    feat: add datasource

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,19 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
-
-const mocks = {
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const TrackAPI = require("./datasources/tracks-api");
+const resolvers = require("./resolvers");
 
 const server = new ApolloServer({
   typeDefs,
-  mocks,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
 });
 
